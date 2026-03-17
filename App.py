4a25864dfdee0b01257873cae4f1d976e2582b8d
Rashmitha Dsouza <your-email@gmail.com>
2026-03-17 10:01:02 +0530

    fix langchain document import

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import fitz
 
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
